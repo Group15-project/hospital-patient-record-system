b99ffd3d0552e06bba6e79b1cb799da72468c245
octoetix <iam8nd9@gmail.com>
2026-06-11 15:34:38 +0100

    feat: Update API base URL for production environment

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL ="https://hospital-patient-record-system-7hcj.onrender.com/api/v1" ;
-///"http://localhost:8080/api/v1"
+
  async function apiRequest(
     endpoint,
     method = "GET",
